8d133161e434f583c8e9a2a9be11d21260e63360
ManuelAlonso <mquesada80111@ufide.ac.cr>
2022-04-25 21:05:37 -0600

    Agregado la generación de token y restablecer contraseña.

--- Base de Datos/DatosIniciales.sql
+++ Base de Datos/DatosIniciales.sql
@@ -3,3 +3,8 @@ use [GoKart];
 insert into TB_RolUsuario
 values('Administrador','Es el admin del negocio'),
 ('Cliente','Cliente basico del sistema');
+
+insert into 
+TB_Usuario
+(cedulaUsuario,primerNombre,primerApellido,correoElectronico,contrasennia,telefono,direccionResidencia,idRol)
+values('116180111','Manuel','Quesada','mquesada80111@ufide.ac.cr',HASHBYTES('SHA2_256','WASD'),'87117042','CASA',1);
