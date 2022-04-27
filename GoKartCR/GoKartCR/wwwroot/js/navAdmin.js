ea96cfec023a8a004d4d4a4ecd4009d877ca4318
Danny2701 <77948072+Danny2701@users.noreply.github.com>
2022-04-26 23:52:05 -0600

    Add files via upload

--- GoKartCR/GoKartCR/wwwroot/js/navAdmin.js
+++ GoKartCR/GoKartCR/wwwroot/js/navAdmin.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function(){
+﻿
+
+$(document).ready(function(){
    
 
     $(".btnEnviar").click(function (e) {
@@ -18,7 +20,39 @@
 
     });
 
+    $('.btnCambiarRol').click(function (e) {
+        e.preventDefault();
+        let row = $(this).closest('tr');
+        let id = row.find('td:eq(0)').text();
+        $.ajax({
+            url: '/Administrador/ActualizarRol',
+            type: 'GET',
+            data: {
+                idUsuario: id
+            },
+            success: function (data) {
+                location.reload();
+            }
+
+
+        })
+
+    });
+
     $("#TablaPreguntas").DataTable({
+
+        language: {
+            url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
+        },
+        pageLength: 8,
+        "order": [[0, "desc"]],
+        dom: 'Bfrtip',
+        buttons: [
+
+        ]
+    });
+
+    $("#TablaUsuario").DataTable({
 
         language: {
             url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
